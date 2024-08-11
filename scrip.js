1e19251dc7cc23a6aa9e52e792b0c34eaf5e63b9
Rushikesh parekar <148846308+Rushi-Parekar11@users.noreply.github.com>
2024-08-11 22:18:06 +0530

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,25 @@
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' 
+    });
+}
+
+function scrollToabout() {
+    window.scrollTo({
+        top: 1560,
+        behavior: 'smooth' 
+    });
+}
+
+function scrollToenquri() {
+    window.scrollTo({
+        top: 2560,
+        behavior: 'smooth' 
+    });
+}
+
+
+function showMessage() {
+            alert("Number is busy!");
+        }
